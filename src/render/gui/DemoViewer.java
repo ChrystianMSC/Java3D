4080ade854af2c2af2b8ecc0a5b2056baf9558a1
ChrystianMSC <chrystianmsc@gmail.com>
2024-02-24 13:36:10 -0300

    Matrix class and manipulation to rotate.

--- src/render/gui/DemoViewer.java
+++ src/render/gui/DemoViewer.java
@@ -29,35 +29,57 @@ public class DemoViewer {
 
                 List<Triangle> tris = new ArrayList<>();
                 tris.add(new Triangle(new Vertex(100, 100, 100),
-                                    new Vertex(-100, -100, 100),
-                                    new Vertex(-100, 100, -100),
-                                    Color.WHITE));
+                                      new Vertex(-100, -100, 100),
+                                      new Vertex(-100, 100, -100),
+                                      Color.WHITE));
                 tris.add(new Triangle(new Vertex(100, 100, 100),
-                                    new Vertex(-100, -100, 100),
-                                    new Vertex(100, -100, -100),
-                                    Color.RED));
+                                      new Vertex(-100, -100, 100),
+                                      new Vertex(100, -100, -100),
+                                      Color.RED));
                 tris.add(new Triangle(new Vertex(-100, 100, -100),
-                                    new Vertex(100, -100, -100),
-                                    new Vertex(100, 100, 100),
-                                    Color.GREEN));
+                                      new Vertex(100, -100, -100),
+                                      new Vertex(100, 100, 100),
+                                      Color.GREEN));
                 tris.add(new Triangle(new Vertex(-100, 100, -100),
-                                    new Vertex(100, -100, -100),
-                                    new Vertex(-100, -100, 100),
-                            Color.BLUE));
+                                      new Vertex(100, -100, -100),
+                                      new Vertex(-100, -100, 100),
+                                      Color.BLUE));
+
+                double heading = Math.toRadians(headingSlider.getValue());
+                Matrix3 headingTransform = new Matrix3(new double[] 
+                    {
+                        Math.cos(heading), 0, Math.sin(heading),
+                        0, 1, 0,
+                        -Math.sin(heading), 0, Math.cos(heading)
+                    });
+                double pitch = Math.toRadians(pitchSlider.getValue());
+                Matrix3 pitchTransform = new Matrix3(new double[] 
+                    {
+                        1, 0, 0,
+                        0, Math.cos(pitch), Math.sin(pitch),
+                        0, -Math.sin(pitch), Math.cos(pitch)
+                    });
+                Matrix3 transform = headingTransform.multiply(pitchTransform);
 
                 g2.translate(getWidth() / 2, getHeight() / 2);
                 g2.setColor(Color.WHITE);
                 for (Triangle t : tris) 
                 {
+                    Vertex v1 = transform.transform(t.v1);
+                    Vertex v2 = transform.transform(t.v2);
+                    Vertex v3 = transform.transform(t.v3);
                     Path2D path = new Path2D.Double();
-                    path.moveTo(t.v1.x, t.v1.y);
-                    path.lineTo(t.v2.x, t.v2.y);
-                    path.lineTo(t.v3.x, t.v3.y);
+                    path.moveTo(v1.x, v1.y);
+                    path.lineTo(v2.x, v2.y);
+                    path.lineTo(v3.x, v3.y);
                     path.closePath();
                     g2.draw(path);
                 }
             }
         };
+        headingSlider.addChangeListener(e -> renderPanel.repaint());
+        pitchSlider.addChangeListener(e -> renderPanel.repaint());
+
         pane.add(renderPanel, BorderLayout.CENTER);
 
         frame.setSize(400, 400);
